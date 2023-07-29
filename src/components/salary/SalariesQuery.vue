02d65fcc91c2b32222dfe298a0c62d41a8d78300
carlosgarciara <carlos.garcia.ra@outlook.com>
2023-07-28 20:51:49 -0600

    fetch salaries by user and by date

--- src/components/salary/SalariesQuery.vue
+++ src/components/salary/SalariesQuery.vue
@@ -1,23 +1,14 @@
 <script setup>
-import { useI18n } from 'vue-i18n';
 import { useSalaryStore } from '@/store/salaryStore';
 import { useEmployeeStore } from '@/store/employeeStore';
 import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
 
-const { t } = useI18n();
-
 const salaryStore = useSalaryStore();
 const { salaryQuery } = storeToRefs(salaryStore);
 
 const userNameQuery = ref('');
-const roles = ref([
-  { name: t('roles.driver'), code: 'driver' },
-  { name: t('roles.loader'), code: 'loader' },
-  { name: t('roles.auxiliares'), code: 'auxiliares' }
-]);
 const employeeStore = useEmployeeStore();
-const selectedUser = ref({});
 const employeesOptions = ref([]);
 const searchByEmployeeName = async (event) => {
   employeesOptions.value = await employeeStore.fetchEmployees(event.query);
@@ -25,9 +16,12 @@ const searchByEmployeeName = async (event) => {
 const selectEmployee = (event) => {
   salaryQuery.value.userId = event.value.id;
 };
-const removedSelectedUser = () => {
+const removeSelectedUser = () => {
   salaryQuery.value.userId = null;
   userNameQuery.value = '';
+};
+const removeDate = () => {
+  salaryQuery.value.date = null;
 };
 
 const loading = ref(false);
@@ -47,7 +41,7 @@ const submit = async () => {
     <div class="grid mt-2">
       <div class="col-12">
         <div class="grid">
-          <div class="col-12">
+          <div class="col-12 lg:col-6">
             <label for="name" class="block text-900 text-xl font-medium mb-2">{{ $t('user.employee') }}</label>
             <div class="p-inputgroup flex-1">
               <AutoComplete
@@ -65,25 +59,18 @@ const submit = async () => {
               >
                 <template #option="{ option }"> {{ option.employeeNumber }} - {{ option.name }}</template>
               </AutoComplete>
-              <Button icon="pi pi-times" size="sm" severity="danger" raised class="p-inputgroup-addon" @click="removedSelectedUser">
+              <Button icon="pi pi-times" size="sm" severity="danger" raised class="p-inputgroup-addon" @click="removeSelectedUser">
                 <!-- <i class="pi pi-times" style="color: #fff"></i> -->
               </Button>
             </div>
           </div>
 
-          <div class="col-12 lg:col-4 md:col-6">
-            <label for="firstName" class="block text-900 text-xl font-medium mb-2">{{ $t('user.role') }}</label>
-            <Dropdown
-              id="role"
-              type="text"
-              placeholder=""
-              class="w-full"
-              disabled
-              v-model="selectedUser.role"
-              optionLabel="name"
-              optionValue="code"
-              :options="roles"
-            />
+          <div class="col-12 lg:col-6">
+            <label for="employeeNumber" class="block text-900 text-xl font-medium mb-2">{{ $t('delivery.date') }}</label>
+            <div class="p-inputgroup flex-1">
+              <Calendar v-model="salaryQuery.date" class="w-full" view="month" dateFormat="mm/yy" />
+              <Button icon="pi pi-times" size="sm" severity="danger" raised class="p-inputgroup-addon" @click="removeDate"> </Button>
+            </div>
           </div>
         </div>
         <div class="flex justify-content-end">
--- src/components/salary/SalariesTable.vue
+++ src/components/salary/SalariesTable.vue
@@ -27,28 +27,28 @@ defineProps({
         <template #body="{ data }"> {{ dayjs(data.createdAt).format('YYYY/MM/DD') }}</template>
       </Column>
       <Column style="width: 7%" field="monthSalary" :header="$t('salary.monthSalary')">
-        <template #body="{ data }"> ${{ data.monthSalary }}</template>
+        <template #body="{ data }"> $ {{ data.monthSalary }}</template>
       </Column>
       <Column style="width: 7%" field="deliveriesBonus" :header="$t('salary.deliveriesBonus')">
-        <template #body="{ data }"> ${{ data.deliveriesBonus }}</template>
+        <template #body="{ data }"> $ {{ data.deliveriesBonus }}</template>
       </Column>
       <Column style="width: 7%" field="bonusByRole" :header="$t('salary.bonusByRole')">
-        <template #body="{ data }"> ${{ data.bonusByRole }}</template>
+        <template #body="{ data }"> $ {{ data.bonusByRole }}</template>
       </Column>
       <Column style="width: 7%" field="subtotalSalary" :header="$t('salary.subtotalSalary')">
-        <template #body="{ data }"> ${{ data.subtotalSalary }}</template>
+        <template #body="{ data }"> $ {{ data.subtotalSalary }}</template>
       </Column>
       <Column style="width: 7%" field="isrRetained" :header="$t('salary.isrRetained')">
-        <template #body="{ data }"> ${{ data.isrRetained }}</template>
+        <template #body="{ data }"> $ {{ data.isrRetained }}</template>
       </Column>
       <Column style="width: 5%" field="isrPercentage" :header="$t('salary.isrPercentage')">
-        <template #body="{ data }"> {{ data.isrPercentage }}&</template>
+        <template #body="{ data }"> {{ data.isrPercentage }} %</template>
       </Column>
       <Column style="width: 7%" field="totalSalary" :header="$t('salary.totalSalary')">
-        <template #body="{ data }"> ${{ data.totalSalary }}</template>
+        <template #body="{ data }"> $ {{ data.totalSalary }}</template>
       </Column>
       <Column style="width: 7%" field="vouchers" :header="$t('salary.vouchers')">
-        <template #body="{ data }"> ${{ data.vouchers }}</template>
+        <template #body="{ data }"> $ {{ data.vouchers }}</template>
       </Column>
     </DataTable>
   </div>
